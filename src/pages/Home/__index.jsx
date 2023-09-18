a8df55a0eee3b4f74bc1fbc1131d5033ad8d51b7
sheldonej <56203719+sheldonej@users.noreply.github.com>
2023-09-18 11:30:11 -0400

    Adding Apple Store Download

--- src/pages/Home/__index.jsx
+++ src/pages/Home/__index.jsx
@@ -66,7 +66,15 @@ function HomePage() {
                             tv:text-left desktop:text-left laptop:text-left  s-laptop:text-center  tablet:text-center  mobile:text-center">
                 Explore, discover, and have fun with your own 3D sound map of the world!
               </p>
-              <a className="h-auto w-fit bg-orange rounded-primary-btn px-16 py-4 text-white text-lg font-semibold" href="https://www.flipcause.com/secure/cause_pdetails/MTc5NDQ1" target="_blank">Donate</a>
+              <div className="flex tv:flex-row desktop:flex-row laptop:flex-row  s-laptop:flex-row  tablet:flex-col  mobile:flex-col gap-6">
+                <a className="h-auto w-52 bg-orange rounded-primary-btn flex justify-center items-center py-4 text-white text-lg font-semibold" href="https://www.flipcause.com/secure/cause_pdetails/MTc5NDQ1" target="_blank">Donate</a>
+                <a className="h-auto w-52 flex-col bg-navy-blue  rounded-primary-btn flex justify-center items-center py-4 text-white text-lg font-semibold" href="https://apps.apple.com/us/app/soundscape-community/id6449701760" target="_blank">
+                  <span className="text-sm">Download on the</span>
+                  Apple Store
+                </a>
+
+              </div>
+             
           </div>
         </div>
       {/* ------------------------------------------------------------------------------------------------------------------------------------------------ */}
